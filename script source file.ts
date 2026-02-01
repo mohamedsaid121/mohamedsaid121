9c0b1f624cb5b77340d6f02556f97ca83268ccf7
Mohamed Said <62230220+mohamedsaid121@users.noreply.github.com>
2026-02-01 17:14:30 +0200

    Update script source file.ts

--- script source file.ts
+++ script source file.ts
@@ -1,12 +1,15 @@
 function main(workbook: ExcelScript.Workbook, sheetName: string): string {
     const sourceFileName = workbook.getName().replace(/\.xlsx$/i, "");
     const SourceColumns = [
-        "Staff ID",
-        "Full Name",
-        "Number of Sick Days",
-        "Diagnosis",
-        "SHW Feedback "
+        "Certificate NBR",
+        "Individual Name"
     ];
+
+    let ExpectedOutputColumns = [
+        "Certificate NBR",
+        "Individual Name",
+        "Diagnosis"
+    ]
 
     let sheet = workbook.getWorksheet(sheetName);
     if (!sheet){
@@ -30,26 +33,68 @@ function main(workbook: ExcelScript.Workbook, sheetName: string): string {
     let headers = rawData[0].map(header => header.trim().toLowerCase());
     let trimmedSourceColumnNames = SourceColumns.map(colName => colName.trim().toLowerCase());
 
-    // columns indices in excel file
-    const columnIndices = trimmedSourceColumnNames.map(colName =>
+    // source columns indices in excel file without diagnosis columns
+    let columnIndices = trimmedSourceColumnNames.map(colName =>
         headers.findIndex(header => header === colName)
     );
-    
+
+    let diagnosisColumnIndices: number[] = [];
+    let diagnosisColumnNames: string [] = [];
+
+    for (let i = 0; i < headers.length; i++){
+        let headerName = headers[i];
+        if (headerName && /^Disease Description(\s*\d*)?$/i.test(headerName)) {
+            diagnosisColumnIndices.push(i);
+            diagnosisColumnNames.push(headerName);
+        }
+    }
 
     let filteredData = rawData.slice(1)
-                              .filter(row => row.every(cell =>
+                              .filter(row => row.some(cell =>
         cell !== "" || cell !== null || cell !== undefined
     ));
 
-    let extractedData = filteredData.map(row =>
-        columnIndices.map(index => index !== -1 ? row[index]: "")
-    );
+
+    let duplicateStaffId = new Set<string>();
+    let extractedData: string[][] = [];
+
+    for (let row in filteredData){
+        let staffId = row[columnIndices[0]];
+        let fulName = row[columnIndices[1]];
+        let diagnoses: string[] = [];
+
+        if (staffId && duplicateStaffId.has(staffId)){
+            continue;
+        }
+        if (staffId){
+            duplicateStaffId.add(staffId);
+        }
+
+        if (diagnosisColumnIndices.length > 0){
+            for (let diagIndex of diagnosisColumnIndices){
+                if(diagIndex < row.length){
+                    let diagnosis = row[diagIndex] || "";
+                    if (diagnosis.trim() !== ""){
+                        diagnoses.push(diagnosis.trim());
+                    }
+                }
+            }
+        }
+        if (diagnoses.length > 0){
+            for (let diagnosis of diagnoses){
+                extractedData.push([staffId, fulName, diagnosis]);
+            }
+        } else {
+            extractedData.push([staffId, fulName, ""]);
+        }
+    }
 
     const result = {
         sourceFileName: sourceFileName,
-        sourceColumns: SourceColumns,
+        sourceColumns: ExpectedOutputColumns,
         data: extractedData
     };
+    console.log(result);
 
     return JSON.stringify(result);
 }
